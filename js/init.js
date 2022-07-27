006c0c2d5cf4c929d66f0afdc6ffcaad9bdaa638
eksa-arifa <81791231+eksa-arifa@users.noreply.github.com>
2022-07-27 16:02:46 +0700

    Delete init.js

--- js/init.js
+++
@@ -1,1 +0,0 @@
-let nama = prompt('Masukkan namamu,tenang saja namamu tidak akan terlihat/disimpan oleh pemilik web,nama ini hanya digunakan untuk inisialisasi,agar Kana bisa tau nama panggilanmu');
